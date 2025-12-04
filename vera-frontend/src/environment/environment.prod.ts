3cb188f601a41873e2c882cb491e2670902cb203
Nour Hannachi <nourhannachi@MacBook-Air-de-Nour-6.local>
2025-12-04 22:51:26 +0100

    package

---
+++ vera-frontend/src/environment/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: "https://vera-api-xxxxx.onrender.com"
+  };
+  
---
+++ vera-frontend/src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: "https://vera-api-xxxxx.onrender.com"
+  };
+  
